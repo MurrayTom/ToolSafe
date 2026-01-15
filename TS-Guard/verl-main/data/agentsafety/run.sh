fafcd8cc8d433065f87dbd66b9dfa843a5dfd967
MurrayTom <163309363+MurrayTom@users.noreply.github.com>
2026-01-15 22:04:40 +0800

    Delete TS-Guard/verl-main directory

--- TS-Guard/verl-main/data/agentsafety/run.sh
+++
@@ -1,6 +0,0 @@
-iter_id=1
-model_path_init=/mnt/shared-storage-user/mouyutao/AShell-ours/verl-main/checkpoints/verl_grpo_labeler_v1-rollout32_iter0/qwen2.5_7b_instruct_function_rm/global_step_5
-
-python iter_self-labeling.py \
-    --iter_id $iter_id \
-    --model_path $model_path_init/actor_hf
--- TS-Guard/verl-main/data/process.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# 读取 Parquet 文件
-df = pd.read_parquet('./agentsafety/test.parquet')
-
-# 将 DataFrame 保存为 JSON 文件
-df.to_json('./agentsafety/test.json', orient='records', lines=True)
-
-print("转换完成！")
